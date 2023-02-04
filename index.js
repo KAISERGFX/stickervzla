103e0b97b3cf76aed825f35f8ba14220cdd1229f
KAISERGFX <101181224+KAISERGFX@users.noreply.github.com>
2023-02-04 17:50:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { create, decryptMedia } = require("@open-wa/wa-automate --socket --tunnel");
+const { create, decryptMedia } = require("@open-wa/wa-automate");
 
 const express = require("express");
 const app = express();
@@ -18,48 +18,11 @@ function start(client) {
   client.onAnyMessage(async (message) => {
     const { isMe, isMedia, caption, from, type, mimetype } = message;
     let stickerMetadata = {
-      pack: "Rey",
+      pack: "Stickers",
       author: "@xkaiserw",
       keepScale: true,
     };
-    if (isMedia && caption?.toLowerCase() === "sticker") {
-      if (mimetype) {
-        const mediaData = await decryptMedia(message);
-        const receiver = isMe ? to : from;
-        if (type === "video") {
-          await client.sendMp4AsSticker(receiver, mediaData, null, stickerMetadata);
-        } else {
-          await client.sendImageAsSticker(
-            receiver,
-            `data:${mimetype};base64,${mediaData.toString("base64")}`,
-            stickerMetadata
-          );
-        }
-      }const { create, decryptMedia } = require("@open-wa/wa-automate");
-
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3030;
-
-// your code
-
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-function start(client) {
-  client.onAnyMessage(async (message) => {
-    const { isMe, isMedia, caption, from, type, mimetype } = message;
-    let stickerMetadata = {
-      pack: "Rey",
-      author: "@xkaiserw",
-      keepScale: true,
-    };
-    if (isMedia && caption?.toLowerCase() === "sticker") {
+    if (isMedia && caption?.toLowerCase() === ".sticker") {
       if (mimetype) {
         const mediaData = await decryptMedia(message);
         const receiver = isMe ? to : from;
@@ -78,8 +41,3 @@ function start(client) {
 }
 
 create().then(start);
-    }
-  });
-}
-
-create().then(start)
